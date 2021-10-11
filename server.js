b4dd2008fcbdc930269b2f2d6843ee93762370b1
Rohitmandal123 <emp91pam@engineerbabu.in>
2021-10-11 14:34:38 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 8082;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to backEnd side');
+});
+
+app.get('/users', (req, res) =>{
+    res.send([{id: 1, name: "Rohit"}, {id: 2, name: "john"}]);
+});
+
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
