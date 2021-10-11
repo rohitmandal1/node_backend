731f521e36030812a4f47c74843156934239c896
Rohitmandal123 <emp91pam@engineerbabu.in>
2021-10-11 16:24:07 +0530

    change port

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+echo $NVM_DIR
 #install node modules
 npm install
 
