4c9ee9f3063526d1704872d5bc797fc75c99b92d
Rohitmandal123 <emp91pam@engineerbabu.in>
2021-10-11 14:53:38 +0530

    node moudles add at git ignore

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/users', (req, res) =>{
     res.send([{id: 1, name: "Rohit"}, {id: 2, name: "john"}]);
 });
 
-app.listen(port, () => console.log(`Server started on PORT ${port}`));
+app.listen(port, () => console.log(`Server started on  PORT ${port}`));
